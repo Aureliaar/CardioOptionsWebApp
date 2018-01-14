bd340b83c4bc0d292d3021279d6ec9f3f917483b
aureon <jacopo.sinigaglia@gmail.com>
2018-01-14 20:55:18 +0100

    Preview 14/01

--- gen.py
+++ gen.py
@@ -2,42 +2,61 @@
 def sanitize(text):
 	return text.replace(" ", "_")
 
-def sreplace(string, name):
-	prefix = sanitize(name)+"-"
+def sreplace(string, name, prefix=""):
+	if prefix=="": prefix = sanitize(name)+"-"
 	string = string.replace("{id-prefix}", prefix)
 	string = string.replace("{name}", name)
 	return string
 
 def T(name):
-	prefix = sanitize(name)
-	Tstring = """<div class="group">
-        <input class="textl" type="text" id="{id-prefix}" required="required"/>
-        <label class="textl" for="{id-prefix}">{name}</label>
-        <div class="bar"></div>"""
-    #replacements
+	string = """<div class="group">
+		<input class="textl" type="text" id="{id-prefix}" required="required"/>
+		<label class="textl" for="{id-prefix}">{name}</label>
+		<div class="bar"></div>
+	</div>"""
+
+	print sreplace(string, name)
 
 def M(Name, Options):
 	string = """		<input type="radio" name="{id-prefix}" id="{id-prefix}{name}" value="{id-prefix}{name}"><label class="{num} col" for="{id-prefix}{name}">{name}</label>"""
-	headerstring = """
+	headerstring = """<form class="radio-form cf">
 <section id = "section-{id-prefix}" class="plan cf">
 	<h2>{id-prefix}:</h2>"""
 	enderstring ="""
-</section>"""
+</section></form>"""
 
+	numdict= {2:"two", 3:"three", 4:"four", 5:"five"}
 	num = len(Options)
 	prefix = sanitize(Name)
 	print headerstring.replace("{id-prefix}", prefix)
-	numString = string.replace("{num}", str(num))
+	numberedString = string.replace("{num}", numdict[num])
 
 	for item in Options:
-		print sreplace(numString, item)
+		print sreplace(numberedString, item, prefix)
 	print enderstring
+
 def C(name):
-	pass
+	string = """<label class="label checkmark_label"> {name}
+		<input  id={id-prefix} class="label__checkbox" type="checkbox" />
+		<span class="label__text">
+	  <span class="label__check">
+		<i class="fa fa-check icon"></i>
+	  </span>
+	</span>
+	</label>"""
+	print sreplace(string, name)
+
 def A(name, options=""):
-	pass
+	if len(options) > 0: name = name + options[0]
+	string = """    <div class="group">
+		<input class="textl" type="text" id="{id-prefix}" value="Autocalc" disabled>
+		<label class="textl" for="{id-prefix}">{name}</label>
+		<div class="bar"></div>
+	</div>"""
+	print sreplace(string, name)
+
 def H(name):
-	print """<h2>{name}</h2>
+	print """<h1>{name}</h1>
 	""".replace("{name}", name)
 
 
