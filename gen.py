15f9655dc38837701ed4d6ffc4a61c07b65b6743
aureon <jacopo.sinigaglia@gmail.com>
2018-01-20 16:53:05 +0100

    Preview 20/01

--- gen.py
+++ gen.py
@@ -1,24 +1,37 @@
 
 def sanitize(text):
 	return text.replace(" ", "_").lower()
+def autocalcReplace(string, autocalcData):
+	name = ""
+	attribute=""
+	if autocalcData!="":
+		for data in autocalcData.split(", "):
+			namestring, useless, weight = data.partition("=")
+			namestring = sanitize(namestring)
+			name+=(namestring+" ")
+			attribute+= """ data-{name}Weight="{weight}" """.replace("{name}", namestring).replace("{weight}", weight)
+	name = name.rstrip()
+	return string.replace("{autocalcClass}", name).replace("{autocalcAttribute}", attribute)
 
-def sreplace(string, name, prefix=""):
+def sreplace(string, name, prefix="", autocalcData=""):
 	if prefix=="": prefix = sanitize(name)
 	string = string.replace("{id-prefix}", prefix)
+	string = string.replace("{sanName}", sanitize(name))
 	string = string.replace("{name}", name)
+	string = autocalcReplace(string, autocalcData)
 	return string
 
-def T(name):
+def T(name, autocalc, etype="text"):
 	string = """<div class="group">
-		<input class="textl" type="text" id="{id-prefix}" required="required"/>
+		<input class="textl" type="{type}" id="{id-prefix}" required="required"/>
 		<label class="textl" for="{id-prefix}">{name}</label>
 		<div class="bar"></div>
 	</div>"""
+	string = string.replace("{type}", etype)
+	print sreplace(string, name, autocalcData=autocalc)
 
-	print sreplace(string, name)
-
-def M(Name, Options):
-	string = """		<input type="radio" name="{id-prefix}" id="{id-prefix}{name}" value="{id-prefix}{name}"><label class="{num} col" for="{id-prefix}{name}">{name}</label>"""
+def M(Name, Options, autocalc):
+	string = """		<input class="MyRadioInput {autocalcClass}" type="radio" name="{id-prefix}" id="{id-prefix}{sanName}" value="{id-prefix}{sanName}"><label class="{num} col" for="{id-prefix}{sanName}">{name}</label>"""
 	headerstring = """<form class="radio-form cf">
 <section id = "section-{id-prefix}" class="plan cf">
 	<h2>{id-prefix}</h2>"""
@@ -32,33 +45,34 @@ def M(Name, Options):
 	numberedString = string.replace("{num}", numdict[num])
 
 	for item in Options:
-		print sreplace(numberedString, item, prefix)
+		print sreplace(numberedString, item, prefix=prefix, autocalcData=autocalc)
 	print enderstring
 
-def C(name):
+def C(name, autocalc):
 	string = """<label class="label checkmark_label"> {name}
-		<input  id={id-prefix} class="label__checkbox" type="checkbox" />
+		<input  id={id-prefix} class="label__checkbox {autocalcClass}" type="checkbox" {autocalcAttribute} />
 		<span class="label__text">
 	  <span class="label__check">
 		<i class="fa fa-check icon"></i>
 	  </span>
 	</span>
 	</label>"""
-	print sreplace(string, name)
+	print sreplace(string, name, autocalcData=autocalc)
 
-def A(name, options=""):
+def A(name, options="", autocalc=""):
 	if len(options) > 0: name = name + options[0]
 	string = """    <div class="group">
-		<input class="textl" type="text" id="{id-prefix}" value="Autocalc" disabled>
+		<input class="textl {autocalcClass}" type="text" id="{id-prefix}" value="Autocalc" {autocalcAttribute} disabled>
 		<label class="textl" for="{id-prefix}">{name}</label>
 		<div class="bar"></div>
 	</div>"""
-	print sreplace(string, name)
+	print sreplace(string, name, autocalcData=autocalc)
 
 def H(name):
 	print """<h1>{name}</h1>
 	""".replace("{name}", name)
-
+def D(name, autocalc=""):
+	T(name, autocalc, "date")
 
 with open("spec.txt") as fin:
 	while True:
@@ -67,6 +81,7 @@ with open("spec.txt") as fin:
 		#if len(line) < 2: continue
 		#preprocessing
 		line = line.rstrip()
+		line, useless, autocalc = line.partition(" | ")
 		code = line[:1]
 		stuff = line[2:].split(", ", 1)
 		text = stuff[0]
@@ -75,15 +90,17 @@ with open("spec.txt") as fin:
 		if (len(stuff) > 1): options = stuff[1].split(", ")
 
 		if (code=="T"):
-			T(text)
+			T(text, autocalc)
 		if (code=="C"):
-			C(text)
+			C(text, autocalc)
 		if (code=="A"):
-			A(text, options)
+			A(text, options, autocalc)
 		if (code=="M"):
-			M(text, options)
+			M(text, options, autocalc)
 		if (code=="H"):
 			H(text)
+		if (code=="D"):
+			D(text)
 
 		
 
--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $('#date_of_birth').on('input', function() {
     //alert("dark side");
     var datestr = $('#date_of_birth').val();
-    var year = datestr.slice(6, 12);
-    $('#age').val(2018-parseInt(year));
-    //TODO
+    var dob = +new Date(datestr);
+    var age = (Date.now() - dob)/ (31557600000);
+    age = Math.floor(age);
+    $('#age').val(age);
+    $('#age').trigger('input');
 });
 $('#height, #weight').on('input', function(){
     var height = parseFloat($('#height').val())/100;
@@ -13,7 +15,7 @@ $('#height, #weight').on('input', function(){
 });
 $('#age, #arterial_hypertension, #abnormal_liver_function, #abnormal_renal_function, #transient_ischemic_attack, #labile_inrs, #bleeding, #alcohol_usage_history, #drug_usage_history').on('input', function(){
    var score = 0;
-   if (parseInt($('#age')) > 65) {score++};
+   if (parseInt($('#age').val()) > 65) {score++};
     if ($('#age').is(":checked")){score++};
     if ($(' #arterial_hypertension').is(":checked")){score++};
     if ($(' #abnormal_liver_function').is(":checked")){score++};
@@ -24,4 +26,19 @@ $('#age, #arterial_hypertension, #abnormal_liver_function, #abnormal_renal_funct
     if ($(' #alcohol_usage_history').is(":checked")){score++};
     if ($(' #drug_usage_history').is(":checked")){score++};
    $('#has-bled-_score').val(score);
+});
+$('.euroscore').on('input', function(){
+    score = 0;
+   $('.euroscore[type="checkbox"]').each(function(){
+      var weight = parseFloat($(this).attr('Data-euroscoreWeight'));
+      if ($(this).is(":checked")){score+=weight;}
+   });
+   var age = parseInt($('#age').val());
+   var agescore = (age-60)/5;
+   agescore = Math.ceil(agescore);
+   agescore = Math.max(0, agescore);
+   score+=agescore;
+   if ($('#urgencyemergency').prop("checked")){score+=2;}
+   if (!($('#weight_of_the_interventionisolated_cabg').is("checked"))){score+=2;}
+   $('#euroscore').val(score);
 });
