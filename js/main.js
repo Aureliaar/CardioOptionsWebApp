481f85217943b3db3529955086350219532a881a
aureon <jacopo.sinigaglia@gmail.com>
2018-11-17 14:50:41 +0100

    Tons of fixes.

--- js/main.js
+++ js/main.js
@@ -7,28 +7,24 @@ $('#date_of_birth').on('input', function() {
     $('#age').val(age);
     $('#age').trigger('input');
 });
-$('#age').on('input', function()   {
-    //TODO recent MI
-});
+
 $('#height, #weight').on('input', function(){
     var height = parseFloat($('#height').val())/100;
     var weight = parseFloat($('#weight').val());
     var bmi = weight / (height * height);
     $('#bmi').val(bmi.toFixed(1));
 });
-$('#age, #arterial_hypertension, #abnormal_liver_function, #abnormal_renal_function, #transient_ischemic_attack, #labile_inrs, #bleeding, #alcohol_usage_history, #drug_usage_history').on('input', function(){
-   var score = 0;
+
+$('.hasbled').on('input', function(){
+    score = 0;
+   $('.hasbled[type="checkbox"]').each(function(){
+      var weight = parseFloat($(this).attr('Data-hasbledWeight'));
+      if ($(this).is(":checked")){score+=weight;}
+   });
    if (parseInt($('#age').val()) > 65) {score++};
-    if ($(' #arterial_hypertension').is(":checked")){score++};
-    if ($(' #abnormal_liver_function').is(":checked")){score++};
-    if ($(' #abnormal_renal_function').is(":checked")){score++};
-    if ($(' #transient_ischemic_attack').is(":checked")){score++};
-    if ($(' #labile_inrs').is(":checked")){score++};
-    if ($(' #bleeding').is(":checked")){score++};
-    if ($(' #alcohol_usage_history').is(":checked")){score++};
-    if ($(' #drug_usage_history').is(":checked")){score++};
    $('#has-bled-_score').val(score);
 });
+
 $('.euroscore').on('input', function(){
     score = 0;
    $('.euroscore[type="checkbox"]').each(function(){
@@ -40,9 +36,19 @@ $('.euroscore').on('input', function(){
    agescore = Math.ceil(agescore);
    agescore = Math.max(0, agescore);
    score+=agescore;
+   var age = parseFloat($('#ef').val());
+   if (ef<50.0){score+=1;}
+   if (ef<30.0){score+=2;
+}
    if ($('#urgencyemergency').prop("checked")){score+=2;}
-   if (!($('#weight_of_the_interventionisolated_cabg').is("checked"))){score+=2;}
+
+   if ($('#weight_of_the_intervention2_procedures').is("checked")){score+=2;}
+   if ($('#weight_of_the_intervention3_procedures').is("checked")){score+=2;}
+
+   if ($('#urgencyemergency').is("checked")){score+=2;}
    if ($('#sexfemale').is("checked")){score+=1;}
+   var screatinine = parseFloat($('#serum_creatinine').val());
+   if (screatinine > 2.27 ){score+=2;}
    $('#euroscore').val(score);
 });
 $('.cha2ds2').on('input', function(){
@@ -55,12 +61,23 @@ $('.cha2ds2').on('input', function(){
     var age = parseInt($('#age').val());
     if (age>74){score++;}
     if (age>64){score++;}
+    
+    var stroke = $('#stroke').is("checked");
+    var tia = $('#tia').is("checked");
+    if (stroke || tia){score+=2;}
 
-    $('#cha2ds2-vasc_score').val(score);
+    var lv_dysfunction = $('#lv_dysfunction').is("checked");
+    var heart_failure = $('#heart_failure').is("checked");
+    if (lv_dysfunction || heart_failure){score+=1;}
+    
+    if ($('#diabetes_mellitustype_i').is("checked")){score+=1;}
+    if ($('#diabetes_mellitustype_ii').is("checked")){score+=1;}
+
+    $('#cha2ds2vasc_score').val(score);
 });
-$('#date_of_event').on('input', function(){
+$('#date_mi').on('input', function(){
 
-    var datestr = $('#date_of_event').val();
+    var datestr = $('#date_mi').val();
     var dob = +new Date(datestr);
     var dayssince = (Date.now() - dob)/ (1000*60*60*24);
     $('#recent_mi').prop("checked", dayssince<90);
